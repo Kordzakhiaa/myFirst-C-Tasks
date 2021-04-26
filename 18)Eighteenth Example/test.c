b61a7a6609033b1734f17954d9f758332dca4194
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-04-26 17:32:52 +0400

    eighteenth example

--- 18)Eighteenth Example/test.c
+++ 18)Eighteenth Example/test.c
@@ -17,7 +17,6 @@ int find_third_max(int array[4][4]){
     int first_max, second_max, third_max;
     first_max = second_max = third_max = 0;
 
-
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
